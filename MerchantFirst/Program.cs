98b65d553435ab7880f5025063acf13ee5eea8f0
Eliud Ortiz <eliud.j.ortiz@gmail.com>
2021-04-06 16:40:39 -0700

    Basic input function

---
+++ MerchantFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MerchantFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer value.");
+            bool validInput = int.TryParse(Console.ReadLine(), out int intVal);
+            while (!validInput)
+            {
+                Console.WriteLine("Please input an INTEGER value.");
+                validInput = int.TryParse(Console.ReadLine(), out intVal);    
+            }
+            Console.WriteLine("Your value is this: " + intVal);
+            
+        }
+    }
+}
