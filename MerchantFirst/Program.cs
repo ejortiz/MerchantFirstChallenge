a16397d72e845bd3647eba5b3db64ddec89bc97e
Eliud Ortiz <eliud.j.ortiz@gmail.com>
2021-04-06 18:30:03 -0700

    Part one of program complete

--- MerchantFirst/Program.cs
+++ MerchantFirst/Program.cs
@@ -13,8 +13,29 @@ namespace MerchantFirst
                 Console.WriteLine("Please input an INTEGER value >=1.");
                 validInput = int.TryParse(Console.ReadLine(), out intVal);    
             }
-            Console.WriteLine("Your value is this: " + intVal);
+            Console.WriteLine("======START=======");
+            for (int i = 1; i <= intVal; i++)
+            {
+                Console.WriteLine(GenerateStringFromValue(i, 
+                    3, "Hot", 5, "Dog"));
+            }
             
+        }
+
+        public static string GenerateStringFromValue(int input, int value1, string text1, int value2, string text2)
+        {
+            string output = "";
+            bool multipleOfVal1 = input % value1 == 0;
+            bool multipleOfVal2 = input % value2 == 0;
+            if (!multipleOfVal1 && !multipleOfVal2)
+                return input.ToString();
+            if (multipleOfVal1)
+                output += text1;
+            if (multipleOfVal2)
+                output += text2;
+            if (multipleOfVal1 && multipleOfVal2)
+                output += " " + input;
+            return output;
         }
     }
 }
