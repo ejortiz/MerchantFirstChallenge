e61c2ddce879a86de6f9f7cd6061de992b5063fc
Eliud Ortiz <eliud.j.ortiz@gmail.com>
2021-04-06 17:21:35 -0700

    check added for values less than 1

--- MerchantFirst/Program.cs
+++ MerchantFirst/Program.cs
@@ -6,11 +6,11 @@ namespace MerchantFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input an integer value.");
+            Console.WriteLine("Please input an integer value >=1.");
             bool validInput = int.TryParse(Console.ReadLine(), out int intVal);
-            while (!validInput)
+            while (!validInput || intVal < 1)
             {
-                Console.WriteLine("Please input an INTEGER value.");
+                Console.WriteLine("Please input an INTEGER value >=1.");
                 validInput = int.TryParse(Console.ReadLine(), out intVal);    
             }
             Console.WriteLine("Your value is this: " + intVal);
