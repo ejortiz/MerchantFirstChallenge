734a76ea1e0ff54621e2ec34f9f0f03aa2aae306
Eliud Ortiz <eliud.j.ortiz@gmail.com>
2021-04-07 01:29:31 -0700

    first iteration of completed program

--- MerchantFirst/Program.cs
+++ MerchantFirst/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using static Microsoft.VisualBasic.FileIO.TextFieldParser;
 namespace MerchantFirst
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string dataDir = Path.Combine(projectDirectory, "data" + Path.DirectorySeparatorChar);
+            Console.WriteLine(dataDir);
+            // PART 1: Console Input
+            RunPart1();
+            
+            // PART 2: Read File
+            RunPart2(dataDir + "SampleInput.tab", "\t", dataDir + "SampleOutput.tab");
+        }
+
+        private static void RunPart1()
         {
             Console.WriteLine("Please input an integer value >=1.");
             bool validInput = int.TryParse(Console.ReadLine(), out int intVal);
@@ -19,9 +37,7 @@ namespace MerchantFirst
                 Console.WriteLine(GenerateStringFromValue(i, 
                     3, "Hot", 5, "Dog"));
             }
-            
         }
-
         private static string GenerateStringFromValue(int input, int value1, string text1, int value2, string text2)
         {
             string output = "";
@@ -36,6 +52,37 @@ namespace MerchantFirst
             if (multipleOfVal1 && multipleOfVal2)
                 output += " " + input;
             return output;
+        }
+
+        private static string GenPassFail(int input1, int input2)
+        {
+            if (input1 % input2 == 0 || input2 % input1 == 0)
+                return "pass";
+            return "fail";
+        }
+
+        private static void RunPart2(string filename, string sep, string outputfile)
+        {
+            List<string> newLines = new List<string>();
+            foreach(var line in File.ReadLines(filename))
+            {     
+                var sepLine = line.Split(sep);
+                bool validInput1 = int.TryParse(sepLine[0], out int val1);
+                bool validInput2 = int.TryParse(sepLine[1], out int val2);
+                if (!validInput1 || !validInput2 || val1 == 0 || val2 == 0)
+                {
+                    newLines.Add(string.Join(sep, sepLine[0], sepLine[1], "error"));
+                    continue;
+                    // throw new InvalidCastException("Could not convert " + 
+                    //                                sepLine[0] + " or " + 
+                    //                                sepLine[1] +
+                    //                                " to string. Please correct input file.");
+                }
+                string result = GenPassFail(val1, val2);
+                string newLine = string.Join(sep, sepLine[0], sepLine[1], result);
+                newLines.Add(newLine);
+            }
+            File.WriteAllLines(outputfile, newLines, Encoding.UTF8);
         }
     }
 }
